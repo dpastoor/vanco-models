92c70dbda4063c2c1af28dca30807d5607cc0fe3
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-23 11:37:43 -0400

    renormalize volume, add IOV and apply random effects to Cl/V

--- models/lo_2010.cpp
+++ models/lo_2010.cpp
@@ -4,12 +4,13 @@
 
 [PARAM] @annotated
 CL    : 1     : Clearance (L/hr)
-V     : 0.572 : Volume (L) 
+V     : 36.6 : Volume (L) 
 WT    : 2.9   : Weight (kg)
 PMA   : 34.8  : post-menstral age (wk)
 SCR   : 0.9   : serum creatinine (mg/dL)
 SGA   : 0     : small-for-gestational-age
 SPIRO : 0     : spironolactone (L/kg)
+OCC   : 1     : Occasion
 
 
 [CMT] @annotated
@@ -22,12 +23,16 @@ ncmt=1, trans=11
 D_CENT = 1; 
 double NORM_WT = 70;
 double NORM_PMA = 30;
-double CLi = CL * pow((WT/NORM_WT), 0.75) * pow((PMA/NORM_PMA), 3.16) * (0.83*SGA + 1.03*(1-SGA))
-double Vi = V * (1-0.344*SPIRO) * WT
+double CLi = CL * pow((WT/NORM_WT), 0.75) * pow((PMA/NORM_PMA), 3.16) * (0.83*SGA + 1.03*(1-SGA))*exp(ECL+IOVCL)
+double Vi = V * (1-0.344*SPIRO) * WT/NORM_WT * exp(EV)
 
-[OMEGA] @annotated
+[OMEGA] @annotated @name IIV
 ECL : 0.042 : Eta on CL
 EV  : 0.0159 : Eta on V
+
+[OMEGA] @annotated @name IOV
+IOVCL : 0.0256 : IOV on CL
+
   
 [SIGMA] @annotated
 PROP : 0.0524 : Proportional error (%CV)
