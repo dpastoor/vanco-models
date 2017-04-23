94aa352675cbe0ee2c755d79871f97a59c80d65c
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-23 11:19:34 -0400

    lo model parameter updates

--- models/lo_2010.cpp
+++ models/lo_2010.cpp
@@ -3,13 +3,13 @@
 // identification of predictors for dosing determination.
 
 [PARAM] @annotated
-CL  : 0.345 : Clearance (L/hr)
-V   : 1.75  : Volume (L) 
-wt  : 2.9   : Weight (kg)
-pma : 34.8  : post-menstral age (wk)
-scr : 0.9   : serum creatinine (mg/dL)
-SGA : 0    : small-for-gestational-age
-spiro : 0   : spironolactone (L/kg)
+CL    : 1     : Clearance (L/hr)
+V     : 0.572 : Volume (L) 
+WT    : 2.9   : Weight (kg)
+PMA   : 34.8  : post-menstral age (wk)
+SCR   : 0.9   : serum creatinine (mg/dL)
+SGA   : 0     : small-for-gestational-age
+SPIRO : 0     : spironolactone (L/kg)
 
 
 [CMT] @annotated
@@ -22,16 +22,16 @@ ncmt=1, trans=11
 D_CENT = 1; 
 double NORM_WT = 70;
 double NORM_PMA = 30;
-double CLi = 1.0 * pow((wt/NORM_WT), 0.75) * pow((pma/NORM_PMA), 3.16) * (0.83*SGA + 1.03*(1-SGA))
-double Vi = 0.572 * (1-0.344*spiro) * wt
+double CLi = CL * pow((WT/NORM_WT), 0.75) * pow((PMA/NORM_PMA), 3.16) * (0.83*SGA + 1.03*(1-SGA))
+double Vi = V * (1-0.344*SPIRO) * WT
 
 [OMEGA] @annotated
-  ECL : 20.5 : Eta on CL
-  EV  : 12.6 : Eta on V
+ECL : 0.042 : Eta on CL
+EV  : 0.0159 : Eta on V
   
-  [SIGMA] @annotated
-  PROP : 22.9 : Proportional error (%CV)
-  ADD  : 1.51  : Additive (SD)
+[SIGMA] @annotated
+PROP : 0.0524 : Proportional error (%CV)
+ADD  : 2.28  : Additive (SD)
   
   [TABLE]
 double CP = CENT/Vi;
@@ -42,8 +42,8 @@ double DV = CP*(1+PROP);
   DV  : plasma concentration (mg/L)
   CLi : Individual Clearance (L/hr)
   Vi  : Individual Volume (L)
-  wt  : Weight (kg)
-  pma : post-menstral age (wk)
-  scr : serum creatinine (mg/dL)
+  WT  : Weight (kg)
+  PMA : post-menstral age (wk)
+  SCR : serum creatinine (mg/dL)
   SGA : small-for-gestational-age
-  spiro : spironolactone (L/kg)
+  SPIRO : spironolactone (L/kg)
