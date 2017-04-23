dbd581b6ad20b9cd7021bb13142080bdaca0538e
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-23 14:19:05 -0400

    more tweaks, add IOV and correlation, fix up CPR some correct residual error

--- models/anderson_2007.cpp
+++ models/anderson_2007.cpp
@@ -13,7 +13,8 @@ IONT  : 0    : ionotrope status
 Kage  : 0.00789 : scaling constant of age on renal function
 Fvent : 1.03 : Scaling factor applied for use of positive pressure artificial ventilation
 Fiont : 1.19 : Scaling factor applied for use of inotropes (use of dopamine)
-
+OCC   : 1     : Occasion
+  
 [CMT] @annotated
 CENT : Central compartment (mg)
 
@@ -25,32 +26,39 @@ D_CENT = 1;
 double NORM_WT = 70;
 double NORM_PMA = 34.8;
 double Fpma = pow(PMA, 3.68)/(pow(PMA, 3.68) + pow(33.3, 3.68));
-double CPR = 516*exp(Kage*((PMA-40)/(52-40)));
+// publication serum creatinine in umol/L however standardizing to mg/dL so 
+// extra division by 88.42
+double CPR = 516*exp(Kage*((PMA-40)/(52-40)))/88.42;
 double CLcr = CPR/SCR;
-double CLi = CL*CLcr*pow(WT/NORM_WT,0.75)*Fpma*(Fvent*exp(VENT));
-double Vi = V*(WT/NORM_WT)*(Fiont*exp(IONT));
+double CLi = CL*CLcr*pow(WT/NORM_WT,0.75)*Fpma*(Fvent*exp(VENT))*exp(ECL + IOVCL);
+double Vi = V*(WT/NORM_WT)*(Fiont*exp(IONT))*exp(EV);
 
-[OMEGA] @annotated
-ECL : 0.187 : Eta on CL
-EV  : 0.194 : Eta on V
+[OMEGA] @annotated @block @correlation @name IIV
+ECL : 0.044       : Eta on CL
+EV  : 0.897 0.039 : Eta on V
+
+[OMEGA] @annotated @name IOV
+IOVCL : 0.0149 : Eta on IOV for CL  
   
-  [SIGMA] @annotated
-PROP : 0.23 : Proportional error (%CV)
-ADD  : 1.4  : Additive (SD)
+[SIGMA] @annotated
+PROP : 0.053 : Proportional error (%CV)
+ADD  : 2.25  : Additive 
   
-  [TABLE]
+[TABLE]
 double CP = CENT/Vi;
 double DV = CP*(1+PROP) + ADD;
 
 [CAPTURE] @annotated
-  CP  : predicted plasma concentration (mg/L)
-  DV  : plasma concentration (mg/L)
-  CLi : Individual Clearance (L/hr)
-  Vi  : Individual Volume (L)
-  WT  : Weight (kg)
-  PMA : post-menstral age (wk)
-  SCR : serum creatinine (mg/dL)
-  VENT  : Ventillation status
-  IONT  : ionotrope status
-  Fvent : Scaling factor applied for use of positive pressure artificial ventilation
-  Fiont : Scaling factor applied for use of inotropes (use of dopamine)
+CP     : predicted plasma concentration (mg/L)
+DV     : plasma concentration (mg/L)
+CLi    : Individual Clearance (L/hr)
+Vi     : Individual Volume (L)
+WT     : Weight (kg)
+PMA    : post-menstral age (wk)
+SCR    : serum creatinine (mg/dL)
+VENT   : Ventillation status
+IONT   : ionotrope status
+Kage   : scaling constant of age on renal function
+Fvent  : Scaling factor applied for use of positive pressure artificial ventilation
+Fiont  : Scaling factor applied for use of inotropes (use of dopamine)
+OCC    : Occasion
