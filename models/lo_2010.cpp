392d025b1b33ddd064bb378bd86773f929c20218
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-23 11:38:53 -0400

    add semi colons and additive error

--- models/lo_2010.cpp
+++ models/lo_2010.cpp
@@ -23,8 +23,8 @@ ncmt=1, trans=11
 D_CENT = 1; 
 double NORM_WT = 70;
 double NORM_PMA = 30;
-double CLi = CL * pow((WT/NORM_WT), 0.75) * pow((PMA/NORM_PMA), 3.16) * (0.83*SGA + 1.03*(1-SGA))*exp(ECL+IOVCL)
-double Vi = V * (1-0.344*SPIRO) * WT/NORM_WT * exp(EV)
+double CLi = CL * pow((WT/NORM_WT), 0.75) * pow((PMA/NORM_PMA), 3.16) * (0.83*SGA + 1.03*(1-SGA))*exp(ECL+IOVCL);
+double Vi = V * (1-0.344*SPIRO) * WT/NORM_WT * exp(EV);
 
 [OMEGA] @annotated @name IIV
 ECL : 0.042 : Eta on CL
@@ -40,7 +40,7 @@ ADD  : 2.28  : Additive (SD)
   
   [TABLE]
 double CP = CENT/Vi;
-double DV = CP*(1+PROP);
+double DV = CP*(1+PROP) + ADD;
 
 [CAPTURE] @annotated
   CP  : predicted plasma concentration (mg/L)
