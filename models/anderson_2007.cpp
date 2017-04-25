06f03034e0d1b2a07c9f35990447bd14c372d356
Devin Pastoor <devin.pastoor@gmail.com>
2017-04-25 12:08:24 -0400

    move internal constants to FIXED to keep annotations

--- models/anderson_2007.cpp
+++ models/anderson_2007.cpp
@@ -15,7 +15,14 @@ Kage      : 0.00789 : scaling constant of age on renal function
 Fvent     : 1.03    : Scaling factor applied for use of positive pressure artificial ventilation
 Finot     : 1.19    : Scaling factor applied for use of inotropes (use of dopamine)
 OCC       : 1       : Occasion
-  
+
+[FIXED] @annotated 
+STD_WT   : 70  : Standard weight (kg)
+STD_CLCR : 6   : Standard creatinine clearance (L/h/70kg)
+STD_CPR  : 516 : Creatinine production rate (umol/h/70kg)
+STD_AGE  : 40  : Standard age (y)
+TERM_PMA : 40  : PMA at full term (wk)
+
 [CMT] @annotated
 CENT : Central compartment (mg)
 
@@ -23,18 +30,11 @@ CENT : Central compartment (mg)
 ncmt=1, trans=11
 
 [MAIN]
-
 D_CENT = 1; 
-double STD_WT = 70; //  Standard weight kg (not "normal" weight)
-double STD_CLCR = 6;  // Standard creatinine clearance L/h/70kg
-double STD_CPR = 516; // Creatinine production rate (umol/h/70kg)
-double STD_AGE = 40;  // Standard age (y)
-double TERM_PMA = 40; // PMA at full term (wk)
-double NORM_WT = 70;
+
 double Fpma = 1/(1+pow(PMA/EMATCL50,-HILLCL)); // more efficient calculation of sigmoid emax
 // publication creatinine production rate in umol/h however standardizing to dg/h (mg/dL=dg/L) so 
 // extra division by 88.42
-
 double CPR = STD_CPR/88.42*exp(Kage*((PMA-TERM_PMA)/52-STD_AGE)); // PMA converted to post-natal age (y)
 double CLcr = CPR/SCR; // units of L/h/70kg
 double Rf = CLcr/STD_CLCR; // Renal function is the ratio of predicted CLCR to standard CLCR of 6 L/hr/70kg
@@ -66,7 +66,7 @@ WT     : Weight (kg)
 PMA    : post-menstrual age (wk)
 SCR    : serum creatinine (mg/dL)
 VENT   : ventilation status
-IONT   : inotrope status
+INOT   : inotrope status
 Kage   : scaling constant of PMA on predicted creatinine production rate
 CPR    : creatinine production rate age related fraction
 CLcr   : creatinine clearance (L/h/70kg) 
