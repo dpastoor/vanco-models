22ba9f1f1b074e8b800a867b924b99414c3bd521
Devin Pastoor <devin.pastoor@gmail.com>
2017-05-15 21:28:48 -0400

    normalize to all caps across params

--- models/frymoyer_2014.cpp
+++ models/frymoyer_2014.cpp
@@ -5,11 +5,9 @@
 [PARAM] @annotated
 CL  : 0.345 : Clearance (L/hr)
 V   : 1.75  : Volume (L) 
-wt  : 2.9   : Weight (kg)
-pma : 34.8  : post-menstral age (wk)
-scr : 0.9   : serum creatinine (mg/dL)
-RDV : 0     : observed dv value (mg/L)
-CID : 0     : Base ID
+WT  : 2.9   : Weight (kg)
+PMA : 34.8  : post-menstral age (wk)
+SCR : 0.9   : serum creatinine (mg/dL)
 
 [CMT] @annotated
 CENT : Central compartment (mg)
@@ -21,28 +19,26 @@ ncmt=1, trans=11
 D_CENT = 1; 
 double NORM_WT = 2.9;
 double NORM_PMA = 34.8;
-double CLi = CL*pow(wt/NORM_WT,0.75)*(1/(1 + pow((pma/NORM_PMA),-4.53)))*pow(1/scr, 0.267)*exp(ECL);
-double Vi = V*(wt/NORM_WT)*exp(EV);
+double CLi = CL*pow(WT/NORM_WT,0.75)*(1/(1 + pow((PMA/NORM_PMA),-4.53)))*pow(1/SCR, 0.267)*exp(ECL);
+double Vi = V*(WT/NORM_WT)*exp(EV);
   
 [OMEGA] @annotated
 ECL : 0.04 : Eta on CL
 EV  : 0.01 : Eta on V
 
 [SIGMA] @annotated
-PROP : 0.04 : Proportional error (%CV)
+PROP : 0.04 : Proportional error (variance)
 ADD  : 1.14  : Additive (SD)
 
 [TABLE]
 double CP = CENT/Vi;
-double DV = CP*(1+PROP);
+double DV = CP*(1+PROP) + ADD;
 
 [CAPTURE] @annotated
 CP  : predicted plasma concentration (mg/L)
 DV  : plasma concentration (mg/L)
 CLi : Individual Clearance (L/hr)
 Vi  : Individual Volume (L)
-wt  :  Weight (kg)
-pma : post-menstral age (wk)
-scr : serum creatinine (mg/dL)
-RDV : observed dv value (mg/L)
-CID : Base ID
+WT  : Weight (kg)
+PMA : post-menstral age (wk)
+SCR : serum creatinine (mg/dL)
